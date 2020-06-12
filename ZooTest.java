558ed5a9f5849303740ac2b68f3d59f483424969
chrishunt1 <chunt4@nd.edu>
2020-06-12 16:57:57 -0400

    First commit

---
+++ ZooTest.java
@@ -0,0 +1,48 @@
+// chunt4
+// jbigej
+
+// Milestone 2
+// Shreya Kumar
+
+import AnimalPack.Animal;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ZooTest{
+	public static void main(String[] args){
+		Animal[] zooArray = new Animal[4096];
+/*		Bullfrog Billy = new Bullfrog();
+		zooArray[0] = Billy;
+		Bullfrog Lily = new Bullfrog();
+		zooArray[1] = Lily;
+		KomodoDragon Charles = new KomodoDragon();
+		zooArray[2] = Charles;
+		KomodoDragon Rose = new KomodoDragon();
+		zooArray[3] = Rose;
+		Lion Lenny = new Lion();
+		zooArray[4] = Lenny;
+		Lion Nala = new Lion();
+		zooArray[5] = Nala;
+		Capybara Emma = new Capybara();
+		zooArray[6] = Emma;
+		Capybara Daniel = new Capybara();
+		zooArray[7] = Daniel;
+		Flamingo Britney = new Flamingo();
+		zooArray[8] = Britney;
+		Flamingo Mike = new Flamingo();
+		zooArray[9] = Mike;
+		Emu Mia = new Emu();
+		zooArray[10] = Mia;
+		Emu Sebastian = new Emu();
+		zooArray[11] = Sebastian; */
+
+		System.out.println("Type exit at any time to end the program");
+		System.out.println("Options :");
+		System.out.println("Add animal(add)");
+		System.out.println("Delete animal(delete)");
+		System.out.println("Display animal(display");
+		System.out.println("");
+		System.out.println("Select action: ");
+	}
+}
