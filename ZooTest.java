b626879f0b2ab989681fb6d4045bc28ec85a4554
Jack <jbigej@nd.edu>
2020-06-12 21:01:08 +0000

    Update ZooTest.java

--- ZooTest.java
+++ ZooTest.java
@@ -9,6 +9,43 @@ import AnimalPack.Animal;
 import java.io.*;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
+public class Animal{
+	final Boolean Breathes = true;
+	String SkinType;
+	String Name;
+	String Sound;
+	String ConservationStatus;
+	String EatingHabit;
+//	Animal[] animalList;
+
+	public abstract void setSkinType();
+	public String getSkinType(){
+		return this.SkinType;
+	}
+	public abstract void setName();
+	public String getName(){
+		return this.Name;
+	}
+	public abstract void setSound();
+	public String getSound(){
+		return this.Sound;
+	}
+	public abstract void setConservationStatus();
+	public String getConservationStatus(){
+		return this.ConservationStatus;
+	}
+	public abstract void setEatingHabit();
+	public String getEatingHabit(){
+		return this.EatingHabit;
+	}
+/*	public final void listAnimals(){
+		
+	} */
+}
+
+
 public class ZooTest{
 	public static void main(String[] args){
 		Animal[] zooArray = new Animal[4096];
